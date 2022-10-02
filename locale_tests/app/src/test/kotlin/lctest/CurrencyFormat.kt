a118626188b1a426b9f354554c2bd644bcf63338
yamavol <yamavol@outlook.com>
2022-10-02 21:31:25 +0900

    Updated test

--- locale_tests/app/src/test/kotlin/lctest/CurrencyFormat.kt
+++ locale_tests/app/src/test/kotlin/lctest/CurrencyFormat.kt
@@ -1,6 +1,7 @@
 package lctest
 
 import org.junit.Test
+import java.text.DecimalFormat
 import java.text.NumberFormat
 import java.util.*
 
@@ -8,7 +9,7 @@ open class CurrencyFormatTestBase {
     enum class SymbolPos {
         Prefix,
         Suffix,
-        Undefined,
+        Unsure,
     }
 
     data class SymbolInfo(
@@ -17,22 +18,25 @@ open class CurrencyFormatTestBase {
         val symbol: String,
         val position: SymbolPos,
         val example: String,
+        val decimal: String,
+        val separator: String,
     )
 
     fun getSymbolInfo(currency: Currency, locale: Locale): SymbolInfo {
         val decimal = 12345678.990
+        val decimalFormatter = NumberFormat.getInstance(locale) as DecimalFormat
         val currencyFormatter = NumberFormat.getCurrencyInstance(locale)
         currencyFormatter.currency = currency
         val symbol = currency.getSymbol(locale)
         val formatText = currencyFormatter.format(decimal)
 
         val symbolPos =
-            if (formatText.startsWith(symbol))
+            if (formatText.startsWith(symbol) || formatText.matches("^(¤|XXX)".toRegex()))
                 SymbolPos.Prefix
-            else if (formatText.endsWith(symbol))
+            else if (formatText.endsWith(symbol) || formatText.matches("(¤|XXX)$".toRegex()))
                 SymbolPos.Suffix
             else
-                SymbolPos.Undefined
+                SymbolPos.Unsure
 
         return SymbolInfo(
             locale = locale,
@@ -40,6 +44,8 @@ open class CurrencyFormatTestBase {
             symbol = symbol,
             position = symbolPos,
             example = formatText,
+            decimal = decimalFormatter.decimalFormatSymbols.decimalSeparator.toString(),
+            separator = decimalFormatter.decimalFormatSymbols.groupingSeparator.toString(),
         )
     }
 }
@@ -88,7 +94,7 @@ class CurrencyFormatTest : CurrencyFormatTestBase() {
     }
 }
 
-class CurrencyCheckSymbolPositionByLang_WarnThisIsHeavy : CurrencyFormatTestBase() {
+class CurrencyCheckSymbolPositionByLang_WARN_Report_is_Heavy : CurrencyFormatTestBase() {
     @Test
     fun checkPosition() {
         val allLocales = Locale.getAvailableLocales()
@@ -181,6 +187,64 @@ class CurrencyCheckSymbolPositionByLang_Summary : CurrencyFormatTestBase() {
                 print(info.example)
                 print('\n')
             }
+        }
+    }
+}
+
+class CurrencyCheckSymbolPositionByLang_Summary2 : CurrencyFormatTestBase() {
+    @Test
+    fun checkPosition() {
+        val allLocales = Locale.getAvailableLocales()
+        val allCurrencies = Currency.getAvailableCurrencies()
+
+        // JPY, USD, ....
+        val currencyList = allCurrencies.sortedBy { it.currencyCode }
+
+        val map = mutableMapOf<String, MutableList<SymbolInfo>>()
+        for (currency in currencyList) {
+            for (locale in allLocales) {
+                val lang = locale.language
+
+                if (locale.country.isBlank())
+                    continue
+
+                if (map[lang] == null)
+                    map[lang] = mutableListOf()
+
+                map[lang]?.add(
+                    getSymbolInfo(currency, locale)
+                )
+            }
+        }
+
+
+        for (pair in map.toSortedMap()) {
+            val lang = pair.key
+            val positionOptions = pair.value
+                .distinctBy { x -> Triple(x.position, x.separator, x.decimal) }
+
+            for ((lc,value) in positionOptions.withIndex()) {
+
+                print(lc)
+                print('\t')
+                print(lang)
+                print('\t')
+                print(value.position)
+                print('\t')
+                print(value.separator)
+                print('\t')
+                print(value.decimal)
+                print('\t')
+                print(value.example)
+                print('\n')
+
+
+
+            }
+
+
+
+
         }
     }
 }
